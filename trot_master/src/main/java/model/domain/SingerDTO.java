10e1f8b0c79cae126b7bc01c6fc7a9aa95d8aa43
andytjdqls <114290855+andytjdqls@users.noreply.github.com>
2025-02-07 17:42:21 +0900

    Feat Login/Register (#2)

--- trot_master/src/main/java/model/domain/SingerDTO.java
+++ trot_master/src/main/java/model/domain/SingerDTO.java
@@ -1,5 +1,23 @@
 package model.domain;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class SingerDTO {
-
+	private int singerId;
+    private String name;
+    private LocalDate debutDate;
+    private String socialMedia; // JSON 형태의 문자열
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private int totalPoint;
 }
